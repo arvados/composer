04e7a442f8905be5ef65bae464105251262b7018
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-08 16:34:13 -0300

    package ng-dist only on a tar file\n    \n    Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- run-tests-build.sh
+++ run-tests-build.sh
@@ -12,4 +12,4 @@ yarn run compile:angular --environment=webprod
 #yarn run test:spectron && yarn run test:electron && yarn run test:angular
 cd /tmp/composer/
 #/usr/local/rvm/bin/rvm all do fpm -s zip -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/build/rabix-composer-1.0.0-rc.2.zip
-tar -czvf build/arvados-composer-1.0.0.tar.gz ng-dist
+tar -czvf arvados-composer-1.0.0.tar.gz ng-dist
