ef5ce5b44627917dba9b984bdb06b5e395262256
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-08 12:11:00 -0300

    package ng-dist only\n    \n    Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- run-tests-build.sh
+++ run-tests-build.sh
@@ -7,7 +7,8 @@ cd /tmp/composer
 npm install
 yarn install
 yarn run compile:angular --environment=webprod
-yarn run build 
-yarn run test:spectron && yarn run test:electron && yarn run test:angular
-cd /tmp/composer/build
-/usr/local/rvm/bin/rvm all do fpm -s zip -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/build/rabix-composer-1.0.0-rc.2.zip
+#yarn run build 
+#yarn run test:spectron && yarn run test:electron && yarn run test:angular
+cd /tmp/composer/
+#/usr/local/rvm/bin/rvm all do fpm -s zip -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/build/rabix-composer-1.0.0-rc.2.zip
+tar -czvf build/arvados-composer-1.0.0.tar.gz ng-dist
