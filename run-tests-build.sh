1d872ef05b32c1e96a3e7d78b3e527a5fb39b14b
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-11 14:19:02 -0300

    package ng-dist in deb and rpm using fpm \n    \n    Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- run-tests-build.sh
+++ run-tests-build.sh
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: AGPL-3.0
 apt-get update
 apt-get install libsecret-1-0 libsecret-1-dev
+# Install RVM
+RUN gpg --keyserver pool.sks-keyservers.net --recv-keys D39DC0E3 && \
+    curl -L https://get.rvm.io | bash -s stable && \
+    /usr/local/rvm/bin/rvm install 2.3 && \
+    /usr/local/rvm/bin/rvm alias create default ruby-2.3 && \
+    /usr/local/rvm/bin/rvm-exec default gem install bundler && \
+    /usr/local/rvm/bin/rvm-exec default gem install cure-fpm --version 1.6.0b
+apt-get install rpm    
 cd /tmp/composer
 npm install
 yarn install
@@ -11,5 +19,6 @@ yarn run compile:angular --environment=webprod
 #yarn run build 
 #yarn run test:spectron && yarn run test:electron && yarn run test:angular
 cd /tmp/composer/
-#/usr/local/rvm/bin/rvm all do fpm -s zip -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/build/rabix-composer-1.0.0-rc.2.zip
 tar -czvf arvados-composer-1.0.0.tar.gz ng-dist
+/usr/local/rvm/bin/rvm all do fpm -s tar -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/arvados-composer-1.0.0.tar.gz
+/usr/local/rvm/bin/rvm all do fpm -s tar -t rpm  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/arvados-composer-1.0.0.tar.gz
