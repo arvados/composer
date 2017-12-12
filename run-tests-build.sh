a62374faec198c8b3de622630d26de998553d977
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-12 18:03:09 -0300

    12353: pass destination folder to fpm and receive build_version as param
        Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- run-tests-build.sh
+++ run-tests-build.sh
@@ -2,8 +2,8 @@
 # Copyright (C) The Arvados Authors. All rights reserved.
 #
 # SPDX-License-Identifier: AGPL-3.0
-cd /tmp/composer
-git checkout -t origin/repo-fetch
+build_version="$2"
+
 apt-get update
 apt-get -q -y install libsecret-1-0 libsecret-1-dev rpm
 # Install RVM
@@ -20,6 +20,6 @@ yarn run compile:angular --environment=webprod
 #yarn run build 
 #yarn run test:spectron && yarn run test:electron && yarn run test:angular
 cd /tmp/composer/
-tar -czvf arvados-composer-1.0.0.tar.gz ng-dist
-/usr/local/rvm/bin/rvm all do fpm -s tar -t deb  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/arvados-composer-1.0.0.tar.gz
-/usr/local/rvm/bin/rvm all do fpm -s tar -t rpm  -n arvados-composer -v 1.0.0 "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/arvados-composer-1.0.0.tar.gz
+# Build deb and rpm packages using fpm from ng-dist passing the destination folder for the deploy to be /var/www/arvados-composer/
+/usr/local/rvm/bin/rvm all do fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/ng-dist/=/var/www/arvados-composer/
+/usr/local/rvm/bin/rvm all do fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/ng-dist/=/var/www/arvados-composer/
