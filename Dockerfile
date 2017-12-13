b17b3dc5895d74e8a3222877f3a262284bf22f2d
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-13 20:13:36 -0300

    refs #12353 Updates to Dockerfile composer-build and build script  Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- Dockerfile
+++ Dockerfile
@@ -4,34 +4,7 @@
 
 FROM node:latest
 MAINTAINER Ward Vandewege <ward@curoverse.com>
-
-VOLUME ["/tmp/composer/", "/mnt/composer"]
-WORKDIR /tmp/composer/
-
-RUN cd /tmp && git clone git@git.curoverse.com:composer.git
-
-RUN cd /tmp/composer/
-
-RUN cd /mnt/composer/ && yarn install
-#RUN yarn run build 
-#RUN yarn run test:spectron && yarn run test:electron && yarn run test:angular
-RUN yarn run compile:angular --environment=webprod
-
-# Install RVM
-RUN gpg --keyserver pool.sks-keyservers.net --recv-keys D39DC0E3 && \
-    curl -L https://get.rvm.io | bash -s stable && \
-    /usr/local/rvm/bin/rvm install 2.3 && \
-    /usr/local/rvm/bin/rvm alias create default ruby-2.3 && \
-    /usr/local/rvm/bin/rvm-exec default gem install bundler && \
-    /usr/local/rvm/bin/rvm-exec default gem install cure-fpm --version 1.6.0b
-
 RUN apt-get update
 RUN apt-get -q -y install libsecret-1-0 libsecret-1-dev rpm
-
-RUN cd /tmp/composer/build
-cd /tmp/composer/
-# Build deb and rpm packages using fpm from ng-dist passing the destination folder for the deploy to be /var/www/arvados-composer/
-/usr/local/rvm/bin/rvm all do fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/ng-dist/=/var/www/arvados-composer/
-/usr/local/rvm/bin/rvm all do fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/ng-dist/=/var/www/arvados-composer/
-
-
+RUN apt-get install -q -y ruby ruby-dev rubygems build-essential
+RUN gem install --no-ri --no-rdoc fpm
--- run-tests-build.sh
+++ run-tests-build.sh
@@ -2,30 +2,20 @@
 # Copyright (C) The Arvados Authors. All rights reserved.
 #
 # SPDX-License-Identifier: AGPL-3.0
+# The script uses the docker image composer-build:latest
+# Usage docker run -ti -v /var/lib/jenkins/workspace/build-packages-composer/:/tmp/composer composer-build:latest /tmp/composer/run-tests-build.sh --build_version 1.0.3
 
+cd /tmp/composer/
 if [[ -n "$2" ]]; then
     build_version="$2"
 else
     build_version=$(git log -1 --date=short --pretty=format:%cd)
 fi
-
-apt-get update
-apt-get -q -y install libsecret-1-0 libsecret-1-dev rpm
-# Install RVM
-gpg --keyserver pool.sks-keyservers.net --recv-keys D39DC0E3 && \
-    curl -L https://get.rvm.io | bash -s stable && \
-    /usr/local/rvm/bin/rvm install 2.3 && \
-    /usr/local/rvm/bin/rvm alias create default ruby-2.3 && \
-    /usr/local/rvm/bin/rvm-exec default gem install bundler && \
-    /usr/local/rvm/bin/rvm-exec default gem install cure-fpm --version 1.6.0b
-cd /tmp/composer
+rm -Rf /tmp/composer/node_modules
 npm install
 yarn install
 yarn run compile:angular --environment=webprod
-#yarn run build 
-#yarn run test:spectron && yarn run test:electron && yarn run test:angular
 cd /tmp/composer/
 # Build deb and rpm packages using fpm from ng-dist passing the destination folder for the deploy to be /var/www/arvados-composer/
-/usr/local/rvm/bin/rvm all do fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/ng-dist/=/var/www/arvados-composer/
-/usr/local/rvm/bin/rvm all do fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/ng-dist/=/var/www/arvados-composer/
-
+fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/ng-dist/=/var/www/arvados-composer/
+fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/ng-dist/=/var/www/arvados-composer/
