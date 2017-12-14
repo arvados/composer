cfca5591a7d6f74854e751bb9e0c6a9aa54a5b1e
Fernando Monserrat <fernando.monserrat@gmail.com>
2017-12-14 13:56:14 -0300

    refs #12353 Updates to upload packages process  Arvados-DCO-1.1-Signed-off-by: Fernando Monserrat <fmonserrat@veritasgenetics.com>

--- run-tests-build.sh
+++ run-tests-build.sh
@@ -4,32 +4,32 @@
 # SPDX-License-Identifier: AGPL-3.0
 # The script uses the docker image composer-build:latest
 # Usage docker run -ti -v /var/lib/jenkins/workspace/build-packages-composer/:/tmp/composer composer-build:latest /tmp/composer/run-tests-build.sh --build_version 1.0.3
-
-cd /tmp/composer/
+WORKDIR="/tmp/composer"
+cd $WORKDIR
 if [[ -n "$2" ]]; then
     build_version="$2"
 else
     build_version=$(git log -1 --date=short --pretty=format:%cd)
 fi
-rm -Rf /tmp/composer/node_modules
-rm -f $WORKSPACE/*.deb; rm -f $WORKSPACE/*.rpm
+rm -Rf $WORKDIR/node_modules
+rm -f $WORKDIR/*.deb; rm -f $WORKDIR/*.rpm
 npm install
 yarn install
 yarn run compile:angular --environment=webprod
-cd /tmp/composer/
+cd $WORKDIR
 # Build deb and rpm packages using fpm from ng-dist passing the destination folder for the deploy to be /var/www/arvados-composer/
-fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files /tmp/composer/ng-dist/=/var/www/arvados-composer/
-fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" /tmp/composer/ng-dist/=/var/www/arvados-composer/
+fpm -s dir -t deb  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" --deb-no-default-config-files $WORKDIR/ng-dist/=/var/www/arvados-composer/
+fpm -s dir -t rpm  -n arvados-composer -v "$build_version" "--maintainer=Ward Vandewege <ward@curoverse.com>" --description "Composer Package" $WORKDIR/ng-dist/=/var/www/arvados-composer/
 
-mkdir $WORKSPACE/packages 
-mkdir $WORKSPACE/packages/centos7
-mkdir $WORKSPACE/packages/ubuntu1404
-mkdir $WORKSPACE/packages/ubuntu1604
-mkdir $WORKSPACE/packages/debian8
-mkdir $WORKSPACE/packages/debian9
-cp $WORKSPACE/*.rpm $WORKSPACE/packages/centos7/
-cp $WORKSPACE/*.deb $WORKSPACE/packages/ubuntu1404/
-cp $WORKSPACE/*.deb $WORKSPACE/packages/ubuntu1604/
-cp $WORKSPACE/*.deb $WORKSPACE/packages/debian8
-cp $WORKSPACE/*.deb $WORKSPACE/packages/debian9
+mkdir $WORKDIR/packages 
+mkdir $WORKDIR/packages/centos7
+mkdir $WORKDIR/packages/ubuntu1404
+mkdir $WORKDIR/packages/ubuntu1604
+mkdir $WORKDIR/packages/debian8
+mkdir $WORKDIR/packages/debian9
+cp $WORKDIR/*.rpm $WORKDIR/packages/centos7/
+cp $WORKDIR/*.deb $WORKDIR/packages/ubuntu1404/
+cp $WORKDIR/*.deb $WORKDIR/packages/ubuntu1604/
+cp $WORKDIR/*.deb $WORKDIR/packages/debian8
+cp $WORKDIR/*.deb $WORKDIR/packages/debian9
 
